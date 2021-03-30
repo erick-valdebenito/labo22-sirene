ea92adb7d6b132c0048138c1b62e7d8f05487e1e
Valdebenito Erick <erick.valdebenitoolivares@heig-vd.ch>
2021-03-30 10:46:29 +0000

    Bon fichier main.

--- main.c
+++ main.c
@@ -1,18 +1,21 @@
+#include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-
-#define VIDER_BUFFER while(getchar() != '\n') // evite boucle infini du scanf
-
+#define VIDER_BUFFER while (getchar() != '\n')  // evite boucle infini du scanf
+#define BUFFER_SIZE 64
+#define POINTS 2000
+#define PI 3.14
 
 typedef struct
 {
-    double a;  // amplitud
-    int f;  // frequence
-    int t;  // temps
-    int b;  // indice variation freq
-    int g;  // freq variation sirene
+    double a;  // amplitud, seulement valeurs positives
+    int f;     // frequence, seulement valeurs positives
+    int b;     // indice variation freq
+    int g;     // freq variation sirene, seulement valeurs positives
+
 } Onde;
 
 bool f_control_saisie(int ret)
@@ -31,18 +34,37 @@ bool f_control_saisie(int ret)
     return validation;
 }
 
+void sirene(Onde onde, char* filename, double* temps)
+{
+    FILE* fp = fopen(filename, "w");
+    if(fp == NULL){
+        printf("Error: impossible to open '%s'", filename);
+        exit(EXIT_FAILURE);
+    }
+    fprintf(fp, "T[s],S(t)\n");
+    for (int i = 0; i < POINTS; i++)
+    {
+        double siren =
+            abs(onde.a) * sin(2 * PI * abs(onde.f) * temps[i] + onde.b * 
+            sin(2 * PI * abs(onde.g) * temps[i]));
+        fprintf(fp, "%0.3lf,%0.2lf\n", temps[i],siren);
+    }
+    fclose(fp);
+}
+
 int main(int argc, char* argv[])
 {
     Onde onde = {0};
     bool saisie_valide;
     int ret = 0;
-    char *out_filename = NULL;
+    char out_filename[BUFFER_SIZE] = {0};
+    double temps[POINTS];
+    double j = 0;
 
-    FILE* fp = stdin;
     do
     {
         printf("Insérer une valeur d'amplitude :");
-        ret = fscanf(fp, "%lf", &onde.a);
+        ret = scanf("%lf", &onde.a);
         VIDER_BUFFER;
         saisie_valide = f_control_saisie(ret);
 
@@ -51,7 +73,8 @@ int main(int argc, char* argv[])
     do
     {
         printf("Insérer une valeur de fréquence de base :");
-        ret = fscanf(fp, "%d", &onde.f);
+        ret = scanf("%d", &onde.f);
+        VIDER_BUFFER;
         saisie_valide = f_control_saisie(ret);
 
     } while (saisie_valide == 0);
@@ -59,7 +82,8 @@ int main(int argc, char* argv[])
     do
     {
         printf("Insérer valeur pour l’indice de la variation de fréquence :");
-        ret = fscanf(fp, "%d", &onde.b);
+        ret = scanf("%d", &onde.b);
+        VIDER_BUFFER;
         saisie_valide = f_control_saisie(ret);
 
     } while (saisie_valide == 0);
@@ -68,20 +92,35 @@ int main(int argc, char* argv[])
     {
         printf("Insérer valeur de la fréquence des variations de la sirène :");
         ret = scanf("%d", &onde.g);
+        VIDER_BUFFER;
         saisie_valide = f_control_saisie(ret);
 
     } while (saisie_valide == 0);
 
+    int saisie_valide1 = 0;
     do
     {
         printf("Ecrire le nom de fichier de sortie :");
-        ret = scanf("%s", out_filename);
-        VIDER_BUFFER;
-        saisie_valide = 0; //f_control_saisie(ret);
 
-    } while (saisie_valide == 0);
+        if (fgets(out_filename, sizeof(out_filename) - 1, stdin) != NULL)
+        {
+            saisie_valide1 = 1;
+            out_filename[strcspn(out_filename, "\n")] = 0;  // remove \n in string
+        }
+        else
+        {
+            printf("\nSaisie incorrect. ex : test.txt ou test.csv\n");
+        }
+    } while (saisie_valide1 == 0);
 
-    printf("Insert 'Amplitud' value :");
+    // Creation vecteur temps
+    for (int i = 0; i < POINTS; i++)
+    {
+        temps[i] = j / 1000;
+        j++;
+    }
+
+    sirene(onde, &out_filename[0], &temps[0]);
 
     exit(EXIT_SUCCESS);
 }
